04b4483e1ec8dc28ffcb1f68985bc634125668ed
Kento-Ishizaki <kentoron1115@gmail.com>
2022-02-18 15:18:11 +0900

    WIP: チャットUIの土台作成

---
+++ src/components/Chat.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Moai from "../assets/images/moai.png";
+import Shuttle from "../assets/images/shuttle.png";
+
+const Chat = (props) => {
+  const isQuestion = props.type === "question";
+  const classes = isQuestion ? "p-chat__row" : "p-chat__reverse";
+
+  return (
+    <ListItem className={classes}>
+      <ListItemAvatar>
+        {isQuestion ? (
+          <Avatar alt="icon" src={Moai} />
+        ) : (
+          <Avatar alt="icon" src={Shuttle} />
+        )}
+      </ListItemAvatar>
+      <div className="p-chat__bubble">{props.text}</div>
+    </ListItem>
+  );
+};
+
+export default Chat;
