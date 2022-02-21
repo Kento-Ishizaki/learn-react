6f181d076fc9a35227f067e9d17c766783f25fb5
Kento-Ishizaki <kentoron1115@gmail.com>
2022-02-21 12:26:57 +0900

    Feat: Slack連携

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # React ハンズオン学習用
+
+## Setup
+
+.env をプロジェクトルートに作成
+
+```
+REACT_APP_SLACK_WEBHOOK_URL="YOUR_WEBHOOK_URL"
+```
+
+```
+docker-compose build
+docker-compose up -d
+```
+
+http://localhost:4001
+をブラウザで表示
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import defaultDataset from "./data/dataset";
 import "./assets/styles/style.css";
 import { AnswersList, Chats } from "./components/index";
+import FormDialog from "./components/forms/FormDialog";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,6 +15,8 @@ export default class App extends React.Component {
       open: false,
     };
     this.selectAnswer = this.selectAnswer.bind(this);
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   displayNextQuestion = (nextQuestionId) => {
@@ -35,13 +38,15 @@ export default class App extends React.Component {
       case nextQuestionId === "init":
         setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
         break;
+      case nextQuestionId === "contact":
+        this.handleClickOpen();
+        break;
       case /^https:\/\/*/.test(nextQuestionId):
         const a = document.createElement("a");
         a.href = nextQuestionId;
         a.target = "_blank";
         a.click();
         break;
-
       default:
         const chats = this.state.chats;
         chats.push({
@@ -67,6 +72,14 @@ export default class App extends React.Component {
     }
   }
 
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
     return (
       <section className="c-section">
@@ -76,6 +89,7 @@ export default class App extends React.Component {
             answers={this.state.answers}
             select={this.selectAnswer}
           />
+          <FormDialog open={this.state.open} handleClose={this.handleClose} />
         </div>
       </section>
     );
